4b07868487d088c3431fa3dc78b4904d94d37d40
Javier Guerra <javier.guerra1001@gmail.com>
2023-06-02 11:14:58 -0700

    style(more about): added image to testimonials to show that it will be added later and coming soon, added more attributions, and fixed styling on carousel

--- src/features/readmore/pages/Testimonials.tsx
+++ src/features/readmore/pages/Testimonials.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
+import ComingSoon from '../../../assets/coming_soon.jpg';
 
+const Wrapper = styled.div``;
 const Testimonials: FC = () => (
-  <div>
-    <div>
-      <div className="flex flex-row w-full justify-center items-center relative">
-        <h2 className="text-3xl font-bold my-3">Testimonials</h2>
-      </div>
+  <Wrapper>
+    <div className="flex flex-row w-full justify-center items-center">
+      <h2 className="text-3xl font-bold my-3">Testimonials</h2>
     </div>
-  </div>
+    <img src={ComingSoon} alt="coming-soon" />
+  </Wrapper>
 );
 
 export default Testimonials;
