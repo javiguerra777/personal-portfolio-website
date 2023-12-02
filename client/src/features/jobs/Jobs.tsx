edc7f88240adeea8010731759f56909364f5f8d1
Javier Guerra <javier.guerra1001@gmail.com>
2023-12-02 01:55:10 -0800

    feat(apollo client): index.tsx added apollo client and provider to application, JobsQuery.tsx setup up simple query in own folder for service, Jobs.tsx usequery to fetch from gql server

--- client/src/features/jobs/Jobs.tsx
+++ client/src/features/jobs/Jobs.tsx
@@ -5,43 +5,61 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component';
-import { nanoid } from '@reduxjs/toolkit';
 import { FcWorkflow } from 'react-icons/fc';
+import { useQuery, gql } from '@apollo/client';
 import SectionTitle from '../../common/style/SectionTitle';
-import MyJobs from './services/MyJobs';
+import { JOBS_QUERY } from './services/JobsQuery';
 
 const JobWrapper = styled.div`
   width: 100%;
   background-color: #28282b;
   flex-grow: 1;
 `;
-const Jobs: FC = () => (
-  <JobWrapper>
-    <motion.div
-      className="pt-10 pb-40 flex flex-col items-center"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-    >
-      <SectionTitle>Work History</SectionTitle>
-      <VerticalTimeline>
-        {MyJobs.map(({ company, description, workDates }) => (
-          <VerticalTimelineElement
-            key={nanoid()}
-            className="vertical-timeline-element--work text-black"
-            dateClassName="text-white"
-            date={workDates}
-            icon={<FcWorkflow />}
-            iconClassName="bg-blue-200"
-          >
-            <h4 className="font-semibold">{company}</h4>
-            <p className="italic">{workDates}</p>
-            <p>{description}</p>
-          </VerticalTimelineElement>
-        ))}
-      </VerticalTimeline>
-    </motion.div>
-  </JobWrapper>
-);
+
+interface Job {
+  _id: string;
+  company: string;
+  workDates: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+}
+
+interface JobsData {
+  jobs: Job[];
+}
+const Jobs: FC = () => {
+  const { loading, error, data } = useQuery<JobsData>(JOBS_QUERY);
+  if (loading) return <p>Loading...</p>;
+  return (
+    <JobWrapper>
+      <motion.div
+        className="pt-10 pb-40 flex flex-col items-center"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+      >
+        <SectionTitle>Work History</SectionTitle>
+        <VerticalTimeline>
+          {data !== undefined &&
+            data.jobs.map((job) => (
+              <VerticalTimelineElement
+                key={job._id}
+                className="vertical-timeline-element--work text-black"
+                dateClassName="text-white"
+                date={job.workDates}
+                icon={<FcWorkflow />}
+                iconClassName="bg-blue-200"
+              >
+                <h4 className="font-semibold">{job.company}</h4>
+                <p className="italic">{job.workDates}</p>
+                <p>{job.description}</p>
+              </VerticalTimelineElement>
+            ))}
+        </VerticalTimeline>
+      </motion.div>
+    </JobWrapper>
+  );
+};
 
 export default Jobs;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,21 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
 import store from './app/store';
 import App from './app/App';
 import './index.css';
 import 'react-vertical-timeline-component/style.min.css';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>,
 );
 
