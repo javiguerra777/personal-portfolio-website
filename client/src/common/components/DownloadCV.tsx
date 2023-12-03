e6c2761fdb7ba271b66036538e1c36dae5967f8c
Javier Guerra <javier.guerra1001@gmail.com>
2023-12-02 22:31:09 -0800

    refactor(resume): use a cloud pdf to allow users to view my most up to date resumes when i upload to the cloud

--- client/src/common/components/DownloadCV.tsx
+++ client/src/common/components/DownloadCV.tsx
@@ -1,15 +1,45 @@
-import React, { FC } from 'react';
-import Resume from '../../assets/Javier_Guerra_Software_Dev.pdf';
+import React, { FC, MouseEvent } from 'react';
+import { useQuery, gql } from '@apollo/client';
 
-const DownloadCV: FC = () => (
-  <a
-    className="bg-zinc-700 text-white px-4 py-2 rounded-full hover:bg-zinc-500 inter"
-    href={Resume}
-    download="Javier Guerra Software Developer Resume"
-    rel="noreferrer"
-  >
-    Download CV
-  </a>
-);
+interface ResumeFile {
+  public_id: string;
+  format: string;
+  version: number;
+  url: string;
+  secure_url: string;
+}
+interface ResumeData {
+  resumeFile: ResumeFile[];
+}
+const GET_RESUME = gql`
+  query {
+    resumeFile {
+      public_id
+      format
+      version
+      url
+      secure_url
+    }
+  }
+`;
+const DownloadCV: FC = () => {
+  const { data, loading } = useQuery<ResumeData>(GET_RESUME);
+  const handleClick = (event: MouseEvent<HTMLAnchorElement>) => {
+    if (data === undefined) {
+      event.preventDefault();
+    }
+  };
+  return (
+    <a
+      className="bg-zinc-700 text-white px-4 py-2 rounded-full hover:bg-zinc-500 inter"
+      href={data !== undefined ? data.resumeFile[0].secure_url : ''}
+      target="_blank"
+      rel="noreferrer"
+      onClick={handleClick}
+    >
+      {loading ? 'Loading...' : 'Download Resume'}
+    </a>
+  );
+};
 
 export default DownloadCV;
