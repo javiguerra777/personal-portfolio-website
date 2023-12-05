e654b42dacbbf620693c203235085075eede7956
Javier Guerra <javier.guerra1001@gmail.com>
2023-12-05 01:51:11 -0800

    feat (server url): set up serverl api url to be fetched from .env file

--- client/src/environment/EnvironmentBaseUrl.ts
+++ client/src/environment/EnvironmentBaseUrl.ts
@@ -2,7 +2,7 @@ const GetEnvironmentUrl = () => {
   if (process.env.NODE_ENV === 'development') {
     return 'http://localhost:4000/';
   }
-  return 'https://api.localhost.com/'; // TODO: change to production url
+  return process.env.REACT_APP_SERVER_URI;
 };
 export const EnvironmentBaseUrl = GetEnvironmentUrl();
 export default {};
